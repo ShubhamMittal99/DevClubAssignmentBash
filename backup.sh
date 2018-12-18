bf2927372d77f7ca784fc0ada81bf820ee8037bc
Shubham Mittal <40098745+bibliophilia@users.noreply.github.com>
2018-12-18 22:52:23 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 touch foo.txt
 cd /home/rakesh/Desktop
-cp -vur $1/. $2/ > foo.txt
-cp -vur $2/. $1/ >> foo.txt
-for line in $(cat foo.txt)
-do
-
-done
-
+cp -vr $1/. $2/ > foo.txt
+cp -vr $2/. $1/ >> foo.txt
+cat foo.txt
+rm foo.txt
